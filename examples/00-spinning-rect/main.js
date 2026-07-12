0d4db4797c3b80816416f3ae41121ebc6950e409
reinhack <reinhack.code@gmail.com>
2026-07-12 15:41:44 -0300

    Syntax error corrected

--- examples/00-spinning-rect/main.js
+++ examples/00-spinning-rect/main.js
@@ -1,30 +1,20 @@
-import {
-	CanvasRenderer,
-	Color,
-	InputManager,
-	Rect,
-	Transform,
-	Vector2,
-} from "https://cdn.jsdelivr.net/gh/reinhackVancheat/Vectra@26aed1a0b05e1f3f0d8cfa7bbaef90deb3ae53b3/lib/index.js";
-
+import { CanvasRenderer, Color, InputManager, Rect, Transform, Vector2, } from "../../lib/index.js";
 const canvas = document.getElementById("canvas");
 const renderer = new CanvasRenderer(canvas);
+renderer.setSize(window.innerWidth, window.innerHeight);
 const inputManager = new InputManager(canvas);
 const transform = new Transform(new Vector2(300, 200), 0, new Vector2(1, 1));
 const rect = new Rect(-100, -100, 200, 200);
 const rectColor = Color.red();
 function update() {
-	renderer.clear(Color.white());
-	inputManager.update();
-	const mousePos = inputManager.getMousePosition();
-	const angle = Math.atan2(
-		mousePos.y - transform.position.y,
-		mousePos.x - transform.position.x,
-	);
-	transform.rotation = angle;
-	renderer.setTransform(transform);
-	renderer.fillRect(rect, rectColor);
-	renderer.resetTransform();
-	requestAnimationFrame(update);
+    renderer.clear();
+    inputManager.update();
+    const mousePos = inputManager.getMousePosition();
+    const angle = Math.atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x);
+    transform.rotation = angle;
+    renderer.setTransform(transform);
+    renderer.fillRect(rect, rectColor);
+    renderer.resetTransform();
+    requestAnimationFrame(update);
 }
 update();
--- examples/00-spinning-rect/main.ts
+++ examples/00-spinning-rect/main.ts
@@ -5,10 +5,11 @@ import {
 	Rect,
 	Transform,
 	Vector2,
-} from "https://cdn.jsdelivr.net/gh/reinhackVancheat/Vectra@26aed1a0b05e1f3f0d8cfa7bbaef90deb3ae53b3/lib/index.js";
+} from "https://cdn.jsdelivr.net/gh/reinhackVancheat/Vectra/lib/index.js";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const renderer = new CanvasRenderer(canvas);
+renderer.setSize(window.innerWidth, window.innerHeight);
 const inputManager = new InputManager(canvas);
 
 const transform = new Transform(new Vector2(300, 200), 0, new Vector2(1, 1));
@@ -16,7 +17,7 @@ const rect = new Rect(-100, -100, 200, 200);
 const rectColor = Color.red();
 
 function update() {
-	renderer.clear(Color.white());
+	renderer.clear();
 	inputManager.update();
 
 	const mousePos = inputManager.getMousePosition();
