e07e7ce3644ab6a5c5106dc7dcc8a0efb428316b
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-09 13:41:58 -0300

    First docs commit

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export { CanvasRenderer } from "./CanvasRenderer.js";
-export { Color } from "./Color.js";
-export { InputManager } from "./InputManager.js";
-export { Matrix3 } from "./Matrix3.js";
-export { Rect } from "./Rect.js";
-export { Transform } from "./Transform.js";
-export { Vector2 } from "./Vector2.js";
